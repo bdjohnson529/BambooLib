df99b1f3ebd074dde8a3bf9798a2ce1818a8ad9f
bdjohnson529 <bdjohnson529@gmail.com>
2020-08-21 09:20:39 -0700

    Update Hashing.py

--- BambooLib/Hashing.py
+++ BambooLib/Hashing.py
@@ -8,12 +8,19 @@ Hashing functions
 author : Ben Johnson
 """
 
-
-# input any string
-# output hashed string
-def generate_uniqueid(input_str):
-	m = hashlib.md5()
-	m.update(input_str.encode('utf-8'))
-	md5_hex = m.hexdigest()
-	
-	return md5_hex
+def hash_string(input_str):
+    """
+    Computes the md5 hash of a string.
+    The md5 hashing function generates a hexadecimal number.
+    Only the final 16 digits of the hex number are converted to a decimal number.
+    This will result in some collisions.
+    
+    :param input_str: Input string
+    :type input_str: str
+    """
+    encoded_str = input_str.encode('utf-8')
+    result = hashlib.md5(encoded_str)
+    hex_num = result.hexdigest().upper()
+    decimal_num = int(hex_num[16:], 16)    
+    
+    return decimal_num
